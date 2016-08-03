85dd97ab39c015fa63f58b8b406964c4d56dd403
Meredith Lampe <mlampe@cs.washington.edu>
2016-08-02 23:55:48 -0700

    tweets

--- tweet.php
+++ tweet.php
@@ -9,6 +9,43 @@
         $reply = $cb->oauth2_token();
         $bearer_token = $reply->access_token;
 
-        print("it works!");
+        \Codebird\Codebird::setBearerToken($bearer_token);
+
+        $queryString = "q=&geocode=37.781157,-122.398720,1mi&count=5";
+
+        $cb->setReturnFormat(CODEBIRD_RETURNFORMAT_ARRAY);
+
+        $neighborhood_locations = fopen("js/geo.json", "r") or die("Unable to open file!");
+        print($neighborhood_locations);
+
+        $locations_file = fread($neighborhood_locations, filesize("js/geo.json"));
+        print($locations_file);
+
+        $locations = json_decode($locations_file, true);
+        fclose($neighborhood_locations);
+
+        $count = 0;
+
+        foreach($locations as $name => $location) {
+            foreach($location as $circle) {
+                $query = "q=&geocode=" . $circle["lat"] . "," . $circle["long"]
+                    . "," . $circle["radius"] . "mi&count=5";
+                $reply = $cb->search_tweets($query, true);
+                print("<pre>");
+                print_r($reply);
+                print("</pre>");
+
+                $count += 1;
+                if ($count > 10) {
+                    break;
+                }
+
+            }
+        }
+
+
+
+
+
 
     ?>
