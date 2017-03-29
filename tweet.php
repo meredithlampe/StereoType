36b757a7cf3e90e2a91320bafb5c23ebc411b21c
Meredith Lampe <mlampe@cs.washington.edu>
2017-03-29 11:04:24 -0700

    cleanup

--- tweet.php
+++ tweet.php
@@ -4,7 +4,7 @@
 
         //authenticate
         require_once ('codebird-php-develop/src/codebird.php');
-        \Codebird\Codebird::setConsumerKey('L9lgozTjiY4RZHfpHG5ucLogP', 'h15MrqDDVFsl3S9adLOGKRNznJDeWEJASZFP9rXJW8Jcox5ptn'); // static, see README
+        \Codebird\Codebird::setConsumerKey(****removed****); // static, see README
         $cb = \Codebird\Codebird::getInstance();
         $reply = $cb->oauth2_token();
         $bearer_token = $reply->access_token;
