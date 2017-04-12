d42ba236c92fe02aa1a9558d11223986271dec08
Meredith Lampe <mlampe@cs.washington.edu>
2017-04-11 18:38:25 -0700

    webpack imports in entry.js

--- entry.js
+++ entry.js
@@ -7,13 +7,14 @@
 
 const TextToSVG = require('text-to-svg');
 require("./js/NeighborhoodGeolocation.js");
-//const GridCache = require("./js/GridCache.js");
+import { getGridCache as getGridCache } from './js/GridCache.js';
 
-var width = 900;
+function main() {
+    var width = 900;
 var height = 600;
 var rotate = [122, 0, 0];
 var center = [0, 47.3097];
-var scale  = 150000;
+var scale = 150000;
 var offset = [1141.329833984375 - 263 + width / 2, 142582.609375 + 30];
 
 //font to be used for text
@@ -67,66 +68,145 @@ var bestplaces;
 //    bestplaces = JSON.parse(this.responseText);
 //    console.log(bestplaces);
 
-bestplaces = {"University District":"Morsel","North Beach":"Golden Gardens Park","Broadview":"Carkeek Park","View Ridge":"Seattle Select Moving","Haller Lake":"The Bridge Coffee House","Olympic Hills":"Man'oushe Express","Cedar Park":"Jebena Cafe","Northgate":"Gyro Hut","Blue Ridge":"Carkeek Park","Maple Leaf":"Cloud City Coffee","Matthews Beach":"Burke-Gilman Trail","Sand Point":"Warren G Magnuson Park - Off Leash Area","Windermere":"Pagliacci Pizza","Pinehurst":"Chaiyo Thai Cuisine","Wedgwood":"Van Gogh Coffeehouse","Wallingford":"Harvest Beat","Fremont":"Paseo Caribbean Food - Fremont","Laurelhurst":"Jak's Grill","Hawthorne Hills":"Center For Spiritual Living","Magnolia":"Discovery Park","Queen Anne":"Storyville Coffee Company","Lower Queen Anne":"Kerry Park","Westlake":"Canlis","Interbay":"Windy City Pie","Eastlake":"Pomodoro","Portage Bay":"Little Lago","Beacon Hill":"Cafetal Quilombo","Georgetown":"Calozzi's Cheesesteaks","Belltown":"Some Random Bar","West Seattle":"The Beer Junction","Seward Park":"Seward Park Audubon Center","Alki":"Alki Beach Park","Fauntleroy":"Lincoln Park","South Park":"Phorale","Arbor Heights":"Alki Sewer","Highland Park":"Wanna Teriyaki & Burger","Columbia City":"La Teranga","South Delridge":"Fresh Flours","High Point":"Tug Inn","Mount Baker":"Tacos El Asadero","Industrial District":"Schooner Exact Brewing Company","Madison Valley":"The Harvest Vine","Madison Park":"Cactus Restaurants","Denny-Blaine":"Viretta Park","Downtown":"Monorail Espresso","Whittier Heights":"Un Bien","Broadmoor":"Washington Park Arboretum","Brighton":"Othello Wok and Teriyaki","Sunset Hill":"Geo's Cuban & Creole Cafe","Rainier Beach":"Redwing Cafe","South Lake Union":"I Love My GFF","Capitol Hill":"Ada's Technical Books and Cafe","First Hill":"George's Sausage & Delicatessen","Meadowbrook":"Tubs Gourmet Subs","Admiral":"Freshy's","North College Park":"Tropicos Breeze","Atlantic":"Wood Shop BBQ","Loyal Heights":"Un Bien","Central District":"Jackson's Catfish Corner","International District":"goPok\u00e9","Roosevelt":"Rain City Burgers","Pioneer Square":"Il Corvo Pasta","Ballard":"Caf\u00e9 Besalu","Roxhill":"The Westy","North Delridge":"Pearls Tea & Cafe","Greenwood":"Valhalla Sandwiches","Leschi":"Meet the Moon","Riverview":"Portside Coffee Company","Montlake":"Fuel","Green Lake":"Green Lake Park","Ravenna":"Ventoux Roasters","Crown Hill":"Wild Mountain Cafe","Madrona":"Bottlehouse","Bitter Lake":"Forno Pizza","Olympic Manor":"Taki's Mad Greek","Victory Heights":"Hydra Clean Northwest","Phinney Ridge":"The Dray","Bryant":"Seattle Sunshine Coffee"};
+bestplaces = {
+    "University District": "Morsel",
+    "North Beach": "Golden Gardens Park",
+    "Broadview": "Carkeek Park",
+    "View Ridge": "Seattle Select Moving",
+    "Haller Lake": "The Bridge Coffee House",
+    "Olympic Hills": "Man'oushe Express",
+    "Cedar Park": "Jebena Cafe",
+    "Northgate": "Gyro Hut",
+    "Blue Ridge": "Carkeek Park",
+    "Maple Leaf": "Cloud City Coffee",
+    "Matthews Beach": "Burke-Gilman Trail",
+    "Sand Point": "Warren G Magnuson Park - Off Leash Area",
+    "Windermere": "Pagliacci Pizza",
+    "Pinehurst": "Chaiyo Thai Cuisine",
+    "Wedgwood": "Van Gogh Coffeehouse",
+    "Wallingford": "Harvest Beat",
+    "Fremont": "Paseo Caribbean Food - Fremont",
+    "Laurelhurst": "Jak's Grill",
+    "Hawthorne Hills": "Center For Spiritual Living",
+    "Magnolia": "Discovery Park",
+    "Queen Anne": "Storyville Coffee Company",
+    "Lower Queen Anne": "Kerry Park",
+    "Westlake": "Canlis",
+    "Interbay": "Windy City Pie",
+    "Eastlake": "Pomodoro",
+    "Portage Bay": "Little Lago",
+    "Beacon Hill": "Cafetal Quilombo",
+    "Georgetown": "Calozzi's Cheesesteaks",
+    "Belltown": "Some Random Bar",
+    "West Seattle": "The Beer Junction",
+    "Seward Park": "Seward Park Audubon Center",
+    "Alki": "Alki Beach Park",
+    "Fauntleroy": "Lincoln Park",
+    "South Park": "Phorale",
+    "Arbor Heights": "Alki Sewer",
+    "Highland Park": "Wanna Teriyaki & Burger",
+    "Columbia City": "La Teranga",
+    "South Delridge": "Fresh Flours",
+    "High Point": "Tug Inn",
+    "Mount Baker": "Tacos El Asadero",
+    "Industrial District": "Schooner Exact Brewing Company",
+    "Madison Valley": "The Harvest Vine",
+    "Madison Park": "Cactus Restaurants",
+    "Denny-Blaine": "Viretta Park",
+    "Downtown": "Monorail Espresso",
+    "Whittier Heights": "Un Bien",
+    "Broadmoor": "Washington Park Arboretum",
+    "Brighton": "Othello Wok and Teriyaki",
+    "Sunset Hill": "Geo's Cuban & Creole Cafe",
+    "Rainier Beach": "Redwing Cafe",
+    "South Lake Union": "I Love My GFF",
+    "Capitol Hill": "Ada's Technical Books and Cafe",
+    "First Hill": "George's Sausage & Delicatessen",
+    "Meadowbrook": "Tubs Gourmet Subs",
+    "Admiral": "Freshy's",
+    "North College Park": "Tropicos Breeze",
+    "Atlantic": "Wood Shop BBQ",
+    "Loyal Heights": "Un Bien",
+    "Central District": "Jackson's Catfish Corner",
+    "International District": "goPok\u00e9",
+    "Roosevelt": "Rain City Burgers",
+    "Pioneer Square": "Il Corvo Pasta",
+    "Ballard": "Caf\u00e9 Besalu",
+    "Roxhill": "The Westy",
+    "North Delridge": "Pearls Tea & Cafe",
+    "Greenwood": "Valhalla Sandwiches",
+    "Leschi": "Meet the Moon",
+    "Riverview": "Portside Coffee Company",
+    "Montlake": "Fuel",
+    "Green Lake": "Green Lake Park",
+    "Ravenna": "Ventoux Roasters",
+    "Crown Hill": "Wild Mountain Cafe",
+    "Madrona": "Bottlehouse",
+    "Bitter Lake": "Forno Pizza",
+    "Olympic Manor": "Taki's Mad Greek",
+    "Victory Heights": "Hydra Clean Northwest",
+    "Phinney Ridge": "The Dray",
+    "Bryant": "Seattle Sunshine Coffee"
+};
 console.log(bestplaces);
 
-    //get width of parent
-    var parentWidth = d3.select(".jumbotron").attr("width");
+//get width of parent
+var parentWidth = d3.select(".jumbotron").attr("width");
 
-    var svg = d3.select(".jumbotron")
-        .attr("id", "mapContainer")
-        .append("svg")
-        .attr("id", "mapSVG")
-        .attr("width", parentWidth)
-        .attr("height", height * 2);
+var svg = d3.select(".jumbotron")
+    .attr("id", "mapContainer")
+    .append("svg")
+    .attr("id", "mapSVG")
+    .attr("width", parentWidth)
+    .attr("height", height * 2);
 
-    //create loader - spinny guy
-    var loadingIndicator = Object.create(LoadingIndicator);
-    loadingIndicator.spin(document.getElementById('mapContainer'));
+//create loader - spinny guy
+var loadingIndicator = Object.create(LoadingIndicator);
+loadingIndicator.spin(document.getElementById('mapContainer'));
 
-    var projection = d3.geo.mercator()
-        .rotate(rotate)
-        .scale(scale)
-        .translate(offset)
-        .precision(.5);
+var projection = d3.geo.mercator()
+    .rotate(rotate)
+    .scale(scale)
+    .translate(offset)
+    .precision(.5);
 
-    var path = d3.geo.path()
-        .projection(projection);
+var path = d3.geo.path()
+    .projection(projection);
 
-    var neighborhoodGroup = svg.append("g")
-        .attr('id', 'neighborhoodGroup');
+var neighborhoodGroup = svg.append("g")
+    .attr('id', 'neighborhoodGroup');
 
-    /*parses json, call back function selects all paths (none exist yet)
-     and joins data (all neighborhoods) with each path. since there are no
-     paths, all data points are waiting in 'update.enter'. calling
-     'enter()' gives us these points, and appends a path for each of them,
-     attributing a path and id to each.*/
+/*parses json, call back function selects all paths (none exist yet)
+ and joins data (all neighborhoods) with each path. since there are no
+ paths, all data points are waiting in 'update.enter'. calling
+ 'enter()' gives us these points, and appends a path for each of them,
+ attributing a path and id to each.*/
 
-    var topoGeometries;
+var topoGeometries;
 
-    d3.json("json/neighborhoods.json", function(error, topology) {
+d3.json("json/neighborhoods.json", function (error, topology) {
 
-        topoGeometries = topojson.object(topology, topology.objects.neighborhoods)
-            .geometries;
+    topoGeometries = topojson.object(topology, topology.objects.neighborhoods)
+        .geometries;
 
-        //generate paths around each neighborhood
-        neighborhoodGroup.selectAll("path")
-            .data(topoGeometries)
-            .enter()
-            .append("path")
-            .attr("d", path)
-            .attr("class", "neighborhoodOutline")
-            .attr("fill", function() {
+    //generate paths around each neighborhood
+    neighborhoodGroup.selectAll("path")
+        .data(topoGeometries)
+        .enter()
+        .append("path")
+        .attr("d", path)
+        .attr("class", "neighborhoodOutline")
+        .attr("fill", function () {
 
-                var colorA = color1[Math.floor(Math.random() * color1.length)];
-                var colorB = color2[Math.floor(Math.random() * color2.length)];
-                var pair = colorA + "" + colorB;
-                var color = "#" + pair + pair + pair;
-                return color;
-            })
-            .attr("id", function (d) {
-                return "n_" + d.id
-            });
+            var colorA = color1[Math.floor(Math.random() * color1.length)];
+            var colorB = color2[Math.floor(Math.random() * color2.length)];
+            var pair = colorA + "" + colorB;
+            var color = "#" + pair + pair + pair;
+            return color;
+        })
+        .attr("id", function (d) {
+            return "n_" + d.id
+        });
 //                .attr("fill", function() {
 //                    var letters = '0123456789ABCDEF'.split('');
 //                    var color = '#';
@@ -136,35 +216,39 @@ console.log(bestplaces);
 //                    return color;
 //                });
 
-        //generate inner paths to append text to
-        neighborhoodGroup.selectAll(".neighborhoodInnerPath")
-            .data(topoGeometries)
-            .enter()
-            .append("path")
-            .attr("neighborhoodBounds", path)
-            .attr("class", "neighborhoodInnerPath")
-            .attr("id", function (d) {
-                return "inner_" + d.id;
-            })
-            .attr("d", function (d) {
+    //generate inner paths to append text to
+    neighborhoodGroup.selectAll(".neighborhoodInnerPath")
+        .data(topoGeometries)
+        .enter()
+        .append("path")
+        .attr("neighborhoodBounds", path)
+        .attr("class", "neighborhoodInnerPath")
+        .attr("id", function (d) {
+            return "inner_" + d.id;
+        })
+        .attr("d", function (d) {
 
-                //get current neighborhood shape - 3d list of coords
-                var pathCoords3d = NeighborhoodParser.get3dPathArray(this, d.type == "MultiPolygon");
+            //get current neighborhood shape - 3d list of coords
+            var pathCoords3d = NeighborhoodParser.get3dPathArray(this, d.type == "MultiPolygon");
 
-                if (pathCoords3d != null) { //coordinates are enough to actually make a shape
-                    console.log("about to run slice alg for neighborhood: " + d.properties.name);
-                    horizontalSliceAlg(svg, pathCoords3d, d, bestplaces[d.properties.name], padding, getGridCache());
-                }
-                //stop spinner--we're done!
-                loadingIndicator.stop();
-                if (GRID_CACHE_OUTPUT) {
-                    //console.log(JSON.stringify(getGridCache()) + "end");
-                }
-                return null;
-            });
+            if (pathCoords3d != null) { //coordinates are enough to actually make a shape
+                console.log("about to run slice alg for neighborhood: " + d.properties.name);
+                horizontalSliceAlg(svg, pathCoords3d, d, bestplaces[d.properties.name], padding, getGridCache(),
+                    USE_GRID_CACHING, displayRectangles, displayBounds, displayText, TEXT_SIZE_MULTIPLIER, font);
+            }
+            //stop spinner--we're done!
+            loadingIndicator.stop();
+            if (GRID_CACHE_OUTPUT) {
+                //console.log(JSON.stringify(getGridCache()) + "end");
+            }
+            return null;
+        });
 
 
-    });
+});
+};
+
+window.onload = main;
 
 //};
 //oReq.open("get", "yelp/getyelp.php", true);
@@ -173,7 +257,9 @@ console.log(bestplaces);
 //slice neighborhood horizontally, then vertically
 //according to length of phrase to get grid over neighborhood.
 //Use inscribed rectangles to fill each grid slot with a letter
-function horizontalSliceAlg(svg, pathCoords3d, d, phrase, padding, gridCache) {
+function horizontalSliceAlg(svg, pathCoords3d, d, phrase, padding, gridCache,
+                            USE_GRID_CACHING, displayRectangles, displayBounds,
+                            displayText, TEXT_SIZE_MULTIPLIER, font) {
 
     console.log("rendering neighborhodd: " + d.properties.name);
     if (d.properties.name == "Mount Baker") { return; }
