15a442a9f5d85bbf95347631ad4f1bcf4701665a
Meredith Lampe <mlampe@cs.washington.edu>
2017-04-24 18:46:56 -0700

    more legend items

--- yelp/refreshyelp.php
+++ yelp/refreshyelp.php
@@ -21,10 +21,17 @@ foreach($locations as $name => $location) { //loop through locations
         'Authorization: Bearer gizMnc5_0GCIymbtjEzcsbpPbow6WiAtcAk9HaivKu3_kZKX7u5TRsS0i7QrbYQBtVXE19evr63JwVryoTaUpAVNQxYDo-Qmzu4V_ym8i5IDulhK4wMpsxaY1R3pWHYx'
         ));
     $result = json_decode(curl_exec($curl));
-    $response[$name] = $result->businesses[0]->name;
+    $response[$name] = array();
+    $response[$name]["bestmatch"] = $result->businesses[0]->name;
+    $response[$name]["categories"] = array();
+    foreach($result->businesses[0]->categories as $category) {
+        array_push($response[$name]["categories"], $category);
+    }
+    $response[$name]["review_count"] = $result->businesses[0]->review_count;
+    $response[$name]["rating"] = $result->businesses[0]->rating;
+    $response[$name]["price"] = $result->businesses[0]->price;
     curl_close($curl);
 }
-
 $outputfile = fopen($outputfilename, "w") or die ("Unable to open file");
 fwrite($outputfile, json_encode($response));
 fclose($outputfile);
