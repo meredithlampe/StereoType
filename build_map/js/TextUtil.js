519b43dd22c5faed789e343eed1618d9b8d3ff9a
Meredith Lampe <mlampe@cs.washington.edu>
2017-06-24 23:19:21 -0700

    adding new point at length library instead of raphael

--- build_map/js/TextUtil.js
+++ build_map/js/TextUtil.js
@@ -2,6 +2,10 @@
  * Created by meredith on 5/25/16.
  */
 
+//const raphael = require('raphael');
+const point_at_length = require('point-at-length');
+const polyk = require('polyk');
+
 module.exports = {
 
     calculateNumLevels: function (aspectRatio, phrase, addtlLevel, forcedHorizontal, orientation) {
@@ -534,7 +538,7 @@ module.exports = {
         var shapeG = svg.append("g");
 
         // canvas for trying out char sizes - who even knows if these are the right dimensions
-        var paper = raphael(-500, -500, 320, 200);
+        //var paper = raphael(-500, -500, 320, 200);
         while (!charToBig) {
 
             // if char is normal,
@@ -712,6 +716,116 @@ module.exports = {
             displayBounds, rectangleId, svg, TEXT_SIZE_MULTIPLIER, font,
             gridUnit, textToSVG, raphael);
 
+    },
+
+    getCharacterAsSVG: function (char, gridUnit, svg, d, tag, padding,
+                                    displayText, displayBounds, TEXT_SIZE_MULTIPLIER,
+                                    font, textToSVG, raphael) {
+
+        var startPathX,
+            startPathY,
+            rectangleId,
+            widthOfSlice,
+            heightOfSlice;
+
+        if (gridUnit[0].angle == 0 || gridUnit[0].angle == 180) {
+            startPathX = gridUnit[0].cx - (gridUnit[0].width / 2);
+            startPathY = gridUnit[0].cy + (gridUnit[0].height / 2);
+            widthOfSlice = gridUnit[0].width;
+            heightOfSlice = gridUnit[0].height;
+        } else { //rectangle angle == 90 || 270
+            startPathX = gridUnit[0].cx - (gridUnit[0].height / 2);
+            startPathY = gridUnit[0].cy + (gridUnit[0].width / 2);
+            widthOfSlice = gridUnit[0].height;
+            heightOfSlice = gridUnit[0].width;
+        }
+
+        //apply padding
+        var paddingScaledWidth = padding * widthOfSlice;
+        var paddingScaledHeight = padding * heightOfSlice;
+        startPathX += paddingScaledWidth;
+        startPathY -= paddingScaledHeight;
+        rectangleId = d.properties.name + "_inner";
+
+        return module.exports.getChar(startPathX, startPathY, char, tag, displayText,
+            displayBounds, rectangleId, svg, TEXT_SIZE_MULTIPLIER, font,
+            gridUnit, textToSVG, raphael);
+
+    },
+
+    getChar: function (startPathX, startPathY,
+                          phrase, k, displayText, displayBounds,
+                          rectangleId, svg, TEXT_SIZE_MULTIPLIER, font,
+                          rectangle, textToSVG, raphael) {
+
+        var textSize = 5;
+
+        var defaultOptions = {
+            "x": startPathX,
+            "y": startPathY,
+            "fontSize": textSize
+        };
+
+        // just use this one for now
+        var bestPath = textToSVG.getD(phrase.toUpperCase(), defaultOptions);
+        var charToBig = false;
+
+        var whileLoopCount = 0;
+        // canvas for trying out char sizes - who even knows if these are the right dimensions
+        //var paper = raphael(-500, -500, 320, 200);
+        while (!charToBig) {
+            whileLoopCount++;
+
+            // if char is normal,
+            // convert text to svg
+            var options = {
+                "x": startPathX,
+                "y": startPathY,
+                "fontSize": textSize
+            };
+
+            const charPath = textToSVG.getD(phrase.toUpperCase(), options);
+            var countOutOfBounds = 0; // count num points found to be outside of the containing polygon
+            var stopIterating = false;
+            //var charPaperPath = paper.path(charPath);
+            var numHitTestPoints = 20;
+            //var totalLength = charPaperPath.getTotalLength();
+            var point_at_length_instance = point_at_length(charPath);
+            var totalLength = point_at_length_instance.length();
+            var lengthIncrement = totalLength * 1.0 / numHitTestPoints;
+
+            // check coords of char path, make sure they are inside of polygon
+            for (var curr = 0; curr < numHitTestPoints; curr++) {
+                //var point = charPaperPath.getPointAtLength(lengthIncrement * curr);
+                var point = point_at_length_instance.at(lengthIncrement * curr);
+                debugger;
+
+                if (!point) {
+                    console.log("found undefined point in appendChar");
+                }
+
+                // is point inside of polygon?
+                if (!point || !polyk.ContainsPoint(rectangle.polygon, point.x, point.y)) {
+                    countOutOfBounds++;
+                    if (countOutOfBounds > 2) {
+                        stopIterating = true;
+                        break;
+                    }
+                }
+            }
+
+            if (!stopIterating) {
+                bestPath = charPath;
+                textSize += 1;
+            } else {
+                charToBig = true;
+            }
+
+        }
+
+        console.log("while loop executed " + whileLoopCount + " times");
+
+        return bestPath;
     },
 
     arrayToPath: function(oneDArray) {
