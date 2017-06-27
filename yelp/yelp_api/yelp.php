00dca644ebc489ed04bfe0d9702a48f2c4e2e8a2
Meredith Lampe <mlampe@cs.washington.edu>
2017-06-27 12:19:47 -0700

    removing api keys

--- yelp/yelp_api/yelp.php
+++ yelp/yelp_api/yelp.php
@@ -1,26 +1,29 @@
 <?php
 
-$outputfilename = "../yelp/output.txt";
-
+$outputfilename = "output.txt";
 
 header("Content-type: application/json");
 // get list of name -> business name (1st in list of 'best match')
+
+// get yelp auth
+$config = fopen("../../config.json", "r") or die("Unable to open config file");
+$config_data = json_decode(fread($config, filesize("../../config.json")), true);
+fclose($config);
 
 // get neighborhood names
 $neighborhood_locations = fopen("../js/geo.json", "r") or die("Unable to open file!");
 $locations_file = fread($neighborhood_locations, filesize("../js/geo.json"));
 $locations = json_decode($locations_file, true);
 fclose($neighborhood_locations);
-$response = array();
 
+// get best match for each neighborhood
+$response = array();
 foreach($locations as $name => $location) { //loop through locations
     echo "working....";
     $encodedname = urlencode($name . ', Seattle, WA');
     $curl = curl_init('https://api.yelp.com/v3/businesses/search?location=' . $encodedname);
     curl_setopt($curl, CURLOPT_RETURNTRANSFER, 1);
-    curl_setopt($curl, CURLOPT_HTTPHEADER, array(
-        'Authorization: Bearer gizMnc5_0GCIymbtjEzcsbpPbow6WiAtcAk9HaivKu3_kZKX7u5TRsS0i7QrbYQBtVXE19evr63JwVryoTaUpAVNQxYDo-Qmzu4V_ym8i5IDulhK4wMpsxaY1R3pWHYx'
-        ));
+    curl_setopt($curl, CURLOPT_HTTPHEADER, array($config_data["yelp"]["auth"]));
     $result = json_decode(curl_exec($curl));
     $response[$name] = $result->businesses[0]->name;
     curl_close($curl);
