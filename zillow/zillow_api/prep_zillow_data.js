b3b1df85bfff7684939a50073390be0acfd8f53b
Meredith Lampe <mlampe@cs.washington.edu>
2017-08-12 10:58:01 -0700

    progress on zillow converter from api response to json

--- zillow/zillow_api/prep_zillow_data.js
+++ zillow/zillow_api/prep_zillow_data.js
@@ -3,7 +3,8 @@
  */
 
 const jsonfile = require('jsonfile');
-const d3 = require('d3');
+const fs = require('fs');
+const xml2js = require('xml2js');
 
 if (!process.argv[2] || !process.argv[3]) {
     console.log("usage: node prep_zillow_data.js <zillow_response_file> <output_file>");
@@ -12,27 +13,43 @@ if (!process.argv[2] || !process.argv[3]) {
 
 const outputfile = process.argv[3];
 
-d3.xml(process.argv[2], function (error_output, zillow) {
+var xml = process.argv[2];
+var parser = new xml2js.Parser();
+fs.readFile(__dirname + '/sample_response.xml', function(error_output, data) {
+    parser.parseString(data, function (error_parse, zillow) {
 
-    debugger;
     if (error_output) {
         console.log(error_output);
         process.exit(1); //failure
     }
 
+    if (error_parse) {
+        console.log(error_parse);
+        process.exit(1); //failure
+    }
+
+    debugger;
+
     // get data from cached zillow api stuff
-    var neighborhoods = zillow
-        .documentElement.getElementsByTagName("response")[0]
-        .getElementsByTagName("list")[0]
-        .getElementsByTagName("region");
+    var neighborhoods = zillow['RegionChildren:regionchildren']
+        .response[0].list[0].region;
+
+    //var neighborhoods = zillow
+    //    .getElementsByTagName("response")[0]
+    //    .getElementsByTagName("list")[0]
+    //    .getElementsByTagName("region");
 
     var zindexes_all = {};
 
     for (var i = 0; i < neighborhoods.length; i++) {
         var neighborhood = neighborhoods[i];
-        var name = neighborhood
-            .getElementsByTagName("name")[0]
-            .childNodes[0].data;
+
+        //var name = neighborhood
+        //    .getElementsByTagName("name")[0]
+        //    .childNodes[0].data;
+        var name = neighborhood;
+
+
         var zindex = neighborhood
             .getElementsByTagName("zindex")[0];
 
@@ -45,4 +62,6 @@ d3.xml(process.argv[2], function (error_output, zillow) {
 
     // write json to file
     jsonfile.writeFileSync(outputfile, zindexes_all);
+
+    });
 });
